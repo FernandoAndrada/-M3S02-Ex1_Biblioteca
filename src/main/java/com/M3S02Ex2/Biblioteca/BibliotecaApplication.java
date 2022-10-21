1ec543973ba7344511e8d5f7ecc334f42befcf20
FernandoAndrada <fernandojcandrada@gmail.com>
2022-10-21 12:11:01 -0300

    Cria Ex1-M3s02Ex1-Biblioteca

---
+++ src/main/java/com/M3S02Ex2/Biblioteca/BibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.M3S02Ex2.Biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+}
